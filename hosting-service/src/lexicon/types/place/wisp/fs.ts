682402e37147be10ec096f9dcf92508f782337ed
nekomimi.pet <ana@nekomimi.pet>
2025-11-05 18:53:54 -0500

    sigh

--- hosting-service/src/lexicon/types/place/wisp/fs.ts
+++ hosting-service/src/lexicon/types/place/wisp/fs.ts
@@ -2,7 +2,7 @@
  * GENERATED CODE - DO NOT MODIFY
  */
 import { type ValidationResult, BlobRef } from '@atproto/lexicon'
-import { CID } from 'multiformats/cid'
+import { CID } from 'multiformats'
 import { validate as _validate } from '../../../lexicons'
 import { type $Typed, is$typed as _is$typed, type OmitKey } from '../../../util'
 
